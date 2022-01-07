f765828201c345e40110374ec81591eea9b4473d
ManuuMT <ema14mt@gmail.com>
2022-01-07 19:24:18 +0000

    Model completed

--- src/models.py
+++ src/models.py
@@ -8,28 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(20), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(20), nullable=False)
+    
+    user_follow = relationship('Follow')
+    user_posts = relationship('Post')
+    user_comments = relationship('Comment')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follow(Base):
+    __tablename__ = 'follow'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer,ForeignKey('user.id'))
+    user_to_id = Column(Integer,ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    post_owner = Column(Integer,ForeignKey('user.id'))
+    post_comments = relationship('Comment')
+    post_media = relationship('Media')
 
-## Draw from SQLAlchemy base
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(150), nullable=False)
+    comment_owner = Column(Integer,ForeignKey('user.id'))
+    post_id = Column(Integer,ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50), nullable=False)
+    url = Column(String(50), nullable=False)
+    post_id = Column(Integer,ForeignKey('post.id'))
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
